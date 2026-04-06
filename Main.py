0885f158f3fa1b752e0a58ba4edc46983cbe6cf8
BlackPaganYT <hooligan.rut@gmail.com>
2026-04-06 21:52:11 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,21 +3,23 @@ import Сurrency_rates
 def main():
     while True:
         try:
-            operation = int(input("\nВведите, что вы хотите сделать (1 — Узнать курс на сегодня; 2 — Конвертер валют в рубли): "))
-            print("_" * 50)
+            operation = int(input("Введите, что вы хотите сделать (1 — Узнать курс на сегодня; 2 — Конвертер валют в рубли): "))
+            print('-' * 100)
             if operation == 1:
                 rates, date = Сurrency_rates.get_currency_rates()
                 if rates:
                     Сurrency_rates.display(rates, date)
-                    print("_" * 50)
+                    print('-' * 100)
                 else:
                     print("Не удалось получить курсы валют.")
             elif operation == 2:
                 rates, _ = Сurrency_rates.get_currency_rates()
                 if rates:
                     rubles = Сurrency_rates.user_rubles()
-                    Сurrency_rates.convert(rubles, rates)
-                    print("_" * 50)
+                    print('-' * 100)
+                    if rubles is not None:
+                        Сurrency_rates.convert(rubles, rates)
+                        print('-' * 100)
                 else:
                     print("Не удалось получить курсы валют.")
             else:
