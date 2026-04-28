6046d32e4ecd6d9630ae1399a55586ace51f2d7f
BlackPaganYT <hooligan.rut@gmail.com>
2026-04-28 10:07:53 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import Сurrency_rates
+import Currency_rates
 
 def main():
     while True:
@@ -6,19 +6,19 @@ def main():
             operation = int(input("Введите, что вы хотите сделать (1 — Узнать курс на сегодня; 2 — Конвертер валют в рубли): "))
             print('-' * 100)
             if operation == 1:
-                rates, date = Сurrency_rates.get_currency_rates()
+                rates, date = Currency_rates.get_currency_rates()
                 if rates:
-                    Сurrency_rates.display(rates, date)
+                    Currency_rates.display(rates, date)
                     print('-' * 100)
                 else:
                     print("Не удалось получить курсы валют.")
             elif operation == 2:
-                rates, _ = Сurrency_rates.get_currency_rates()
+                rates, _ = Currency_rates.get_currency_rates()
                 if rates:
-                    rubles = Сurrency_rates.user_rubles()
+                    rubles = Currency_rates.user_rubles()
                     print('-' * 100)
                     if rubles is not None:
-                        Сurrency_rates.convert(rubles, rates)
+                        Currency_rates.convert(rubles, rates)
                         print('-' * 100)
                 else:
                     print("Не удалось получить курсы валют.")
@@ -27,5 +27,13 @@ def main():
         except ValueError:
             print("Вы ввели некорректное значение!")
 
+def start():
+    print("!!Добро пожаловать!!")
+    print("!!Это конвертер валют!!")
+    print("!!Он получает курсы валют из Центрального банка РФ!!")
+    print("!!Он может вывести курсы и также перевести рубли в поддерживаемые валюты!!")
+    print('-' * 100)
+
 if __name__ == '__main__':
+    start()
     main()
